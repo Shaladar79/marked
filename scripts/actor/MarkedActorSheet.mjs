c4cc987b6b68b14cbc061aaa2fc7d057fe4f0029
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-11-18 20:24:50 -0600

    Update MarkedActorSheet.mjs

--- scripts/actor/MarkedActorSheet.mjs
+++ scripts/actor/MarkedActorSheet.mjs
@@ -4,7 +4,7 @@ export class MarkedActorSheet extends ActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["marked", "sheet", "actor"],
-      template: "systems/Marked/templates/actor/actor-sheet.hbs",
+      template: "systems/marked/templates/actor/actor-sheet.hbs",
       width: 900,
       height: 700,
       tabs: [
